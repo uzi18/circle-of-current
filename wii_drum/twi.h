279127ca50632979e7c02056908383ea4697553b
frank.zhao.main <frank.zhao.main@3740e0d8-ef4b-11dd-89c5-9df526a60542>
2009-01-31 04:04:31 +0000

    first upload, still atmega644

---
+++ wii_drum/twi.h
@@ -0,0 +1,17 @@
+#ifndef twi_h
+
+#include <string.h>
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/twi.h>
+
+#include "macros.h"
+#include "pindef.h"
+
+void twi_slave_init(unsigned char);
+void twi_attachRxEvent(void (*)(unsigned char *, unsigned char));
+void twi_attachTxEvent(void (*)(void));
+void twi_transmit(unsigned char *, unsigned char);
+
+#define twi_h
+#endif
