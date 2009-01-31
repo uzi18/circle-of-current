eed35248a12c64176bf0e187ed1dce7d6790e9da
frank.zhao.main <frank.zhao.main@3740e0d8-ef4b-11dd-89c5-9df526a60542>
2009-01-31 04:04:25 +0000

    first upload, still atmega644

---
+++ wii_drum/ser.h
@@ -0,0 +1,46 @@
+#ifndef ser_h
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "macros.h"
+
+// Name Replacements
+
+#define UDR UDR0
+#define UCSRA UCSR0A
+#define UCSRB UCSR0B
+#define UCSRC UCSR0C
+#define UBRRH UBRR0H
+#define UBRRL UBRR0L
+#define RXCIE RXCIE0
+#define RXEN RXEN0
+#define TXEN TXEN0
+#define UDRE UDRE0
+#define USART_RX_vect USART0_RX_vect
+
+#define serRxBufferSize 8
+
+// functions
+
+unsigned int getBaudRate(unsigned long);
+
+void serInit(unsigned long);
+
+void serFlush(void);
+
+unsigned char serRx(unsigned char *);
+
+void serSkipTo(unsigned char);
+
+unsigned char serAvail(unsigned char *, unsigned char *);
+
+void serTx(unsigned char);
+
+static int ser_putchar(unsigned char, FILE *);
+
+static FILE serStdout = FDEV_SETUP_STREAM(ser_putchar, NULL, _FDEV_SETUP_WRITE);
+
+#define ser_h
+#endif
