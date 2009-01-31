6798defa8f406e51c341d23b24c7706fcd92e353
frank.zhao.main <frank.zhao.main@3740e0d8-ef4b-11dd-89c5-9df526a60542>
2009-01-31 04:38:23 +0000

    first check in, still a few TODOs

---
+++ gps_logger/ser.h
@@ -0,0 +1,47 @@
+#ifndef ser_inc
+
+#include <avr/io.h>
+#include <avr/wdt.h>
+#include <avr/interrupt.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "macros.h"
+
+// Name Replacements
+#ifndef __AVR_ATmega32__
+#define UDR UDR0
+#define UCSRA UCSR0A
+#define UCSRB UCSR0B
+#define UCSRC UCSR0C
+#define UBRRH UBRR0H
+#define UBRRL UBRR0L
+#define RXCIE RXCIE0
+#define RXEN RXEN0
+#define TXEN TXEN0
+#define UDRE UDRE0
+#define USART_RXC_vect USART0_RX_vect
+#endif
+
+// variables
+
+#define serRxBufferSize 150
+volatile unsigned char serRxBuffer[serRxBufferSize];
+volatile unsigned char serRxBufferHead;
+volatile unsigned char serRxBufferTail;
+
+// functions
+
+void serInit(unsigned long);
+
+//void serFlush(void);
+
+unsigned char serRx(unsigned char *);
+
+void serSkipTo(unsigned char);
+
+//unsigned char serAvail(unsigned char *, unsigned char *);
+
+void serTx(unsigned char);
+
+#define ser_inc
+#endif
