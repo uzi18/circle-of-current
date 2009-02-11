e481b074656a3656739b777e335cd6f2469e025f
frank.zhao.main <frank.zhao.main@3740e0d8-ef4b-11dd-89c5-9df526a60542>
2009-02-11 22:40:52 +0000

    Wii Drum: comments and fixes for TWI engine

--- wii_drum/twi.c
+++ wii_drum/twi.c
@@ -1,28 +1,22 @@
 #include "twi.h"
 
-static volatile unsigned char twi_state;
-static volatile unsigned char twi_slarw;
-
+// pointers to user functions
 static void (*twi_tx_start_event)(unsigned char);
 static void (*twi_tx_end_event)(unsigned char, unsigned char);
 static void (*twi_rx_event)(unsigned char, unsigned char);
 
+// virtual register
 static volatile unsigned char twi_reg[256];
-static volatile unsigned char twi_reg_pointer;
+static volatile unsigned char twi_reg_addr;
 
-static volatile unsigned char twi_error;
-
-static volatile unsigned char twi_first_addr_flag;
-static volatile unsigned char twi_rw_len;
+static volatile unsigned char twi_first_addr_flag; // set address flag
+static volatile unsigned char twi_rw_len; // length of most recent operation
 
 void twi_slave_init(unsigned char addr)
 {
 	// initialize stuff
-	twi_reg_pointer = 0;
+	twi_reg_addr = 0;
 	for(unsigned int i = 0; i < 256; i++) twi_reg[i] = 0;	
-	twi_attach_rx_event(twi_event_empty_2);
-	twi_attach_tx_start(twi_event_empty_1);
-	twi_attach_tx_end(twi_event_empty_2);
 
 	// set slave address
 	TWAR = addr << 1;
@@ -31,6 +25,7 @@ void twi_slave_init(unsigned char addr)
 	TWCR = _BV(TWEN) | _BV(TWIE) | _BV(TWEA);
 }
 
+// access functions for virtual register
 void twi_set_reg(unsigned char addr, unsigned char d)
 {
 	twi_reg[addr] = d;
@@ -41,6 +36,7 @@ unsigned char twi_read_reg(unsigned char addr)
 	return twi_reg[addr];
 }
 
+// event linking functions
 void twi_attach_rx_event( void (*function)(unsigned char, unsigned char) )
 {
 	twi_rx_event = function;
@@ -85,51 +81,49 @@ ISR(TWI_vect)
 			break;
 		case TW_SR_DATA_ACK: // data received, returned ack
 		case TW_SR_GCALL_DATA_ACK: // data received generally, returned ack
-		// put byte in register and ack
 		if(twi_first_addr_flag != 0)
 		{
-			twi_reg[twi_reg_pointer++] = TWDR;
+			// put byte in register
+			twi_reg[twi_reg_addr++] = TWDR;
 			twi_rw_len++;
 		}
 		else
 		{
-			twi_reg_pointer = TWDR;
+			// set address
+			twi_reg_addr = TWDR;
 			twi_first_addr_flag = 1;
 			twi_rw_len = 0;
 		}
-		twi_clear_int(1);
+		twi_clear_int(1); // ack
 			break;
 		case TW_SR_STOP: // stop or repeated start condition received
 			// run user defined function
-			twi_rx_event(twi_reg_pointer - twi_rw_len, twi_rw_len);
-			// ack future responses
-			twi_clear_int(1);
+			twi_rx_event(twi_reg_addr - twi_rw_len, twi_rw_len);
+			twi_clear_int(1); // ack future responses
 			break;
 		case TW_SR_DATA_NACK: // data received, returned nack
 		case TW_SR_GCALL_DATA_NACK: // data received generally, returned nack
-			// nack back at master
-			twi_clear_int(0);
+			twi_clear_int(0); // nack back at master
 			break;
 		
 		// Slave Tx
 		case TW_ST_SLA_ACK:	// addressed, returned ack
 		case TW_ST_ARB_LOST_SLA_ACK: // arbitration lost, returned ack
 			// run user defined function
-			twi_tx_start_event(twi_reg_pointer);
+			twi_tx_start_event(twi_reg_addr);
 			twi_rw_len = 0;
 		case TW_ST_DATA_ACK: // byte sent, ack returned
 			// ready output byte
-			TWDR = twi_reg[twi_reg_pointer++];
+			TWDR = twi_reg[twi_reg_addr++];
 			twi_rw_len++;
-			// ack
-			twi_clear_int(1);
+			twi_clear_int(1); // ack
 			break;
 		case TW_ST_DATA_NACK: // received nack, we are done 
 		case TW_ST_LAST_DATA: // received ack, but we are done already!
 			// ack future responses
 			twi_clear_int(1);
 			// run user defined function
-			twi_tx_end_event(twi_reg_pointer - twi_rw_len, twi_rw_len);
+			twi_tx_end_event(twi_reg_addr - twi_rw_len, twi_rw_len);
 			break;
 	}
 }
--- wii_drum/twi.h
+++ wii_drum/twi.h
@@ -5,6 +5,7 @@
 #include <avr/interrupt.h>
 #include <util/twi.h>
 
+// function prototypes
 void twi_slave_init(unsigned char);
 void twi_set_reg(unsigned char, unsigned char);
 unsigned char twi_read_reg(unsigned char);
