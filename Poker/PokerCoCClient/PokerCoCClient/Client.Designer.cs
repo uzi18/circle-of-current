3269af2d2f2baf7bcf12f4a5a3ef8caf90dca71b
mkman84 <mkman84@3740e0d8-ef4b-11dd-89c5-9df526a60542>
2009-06-12 03:21:46 +0000

    Poker Project: Got netstream working.

--- Poker/PokerCoCClient/PokerCoCClient/Client.Designer.cs
+++ Poker/PokerCoCClient/PokerCoCClient/Client.Designer.cs
@@ -29,8 +29,6 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.listBox2 = new System.Windows.Forms.ListBox();
@@ -47,29 +45,11 @@
             this.label1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.HotTrack;
-            this.label1.Location = new System.Drawing.Point(6, 9);
+            this.label1.Location = new System.Drawing.Point(9, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(102, 31);
+            this.label1.Size = new System.Drawing.Size(147, 31);
             this.label1.TabIndex = 1;
-            this.label1.Text = "Label 1";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.ForeColor = System.Drawing.Color.RoyalBlue;
-            this.label2.Location = new System.Drawing.Point(12, 55);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "label2";
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(54, 47);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(140, 20);
-            this.textBox1.TabIndex = 3;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.label1.Text = "CoC Poker";
             // 
             // button1
             // 
@@ -148,12 +128,11 @@
             this.Controls.Add(this.listBox2);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Client";
             this.Text = "Circle of Current Poker!";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Client_FormClosed);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -163,8 +142,6 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.ListBox listBox2;
--- Poker/PokerCoCClient/PokerCoCClient/Client.cs
+++ Poker/PokerCoCClient/PokerCoCClient/Client.cs
@@ -15,20 +15,33 @@ namespace PokerCoCClient
     public partial class Client : Form
     {
         private string username; 
+        //Connect to server
+        string address = "5.163.58.64";
+
+        TcpClient tcpclnt = new TcpClient();
 
         public Client(string username_)
         {
             InitializeComponent();
             this.username = username_;
 
-            //Connect to server
-            string address = "129.97.209.42";
-
-            TcpClient tcpclnt = new TcpClient();
             tcpclnt.Connect(address, 5000);
-
             Stream netstream = tcpclnt.GetStream();
+            string a = "";
 
+            byte[] bb = new byte[100];
+            int k = netstream.Read(bb, 0, 4);
+            for (int i = 0; i < k; i++)
+                a += Convert.ToChar(bb[i]);
+
+            MessageBox.Show(a);
+
+            ASCIIEncoding asen = new ASCIIEncoding();
+            byte[] bl = new byte[1];
+            bl[0] = (byte)username.Length;
+            byte[] ba = asen.GetBytes(this.username);
+            netstream.Write(bl, 0, 1);
+            netstream.Write(ba, 0, ba.Length);
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -39,6 +52,11 @@ namespace PokerCoCClient
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
             
+        }
+
+        private void Client_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            tcpclnt.Close();
         }
     }
 }
